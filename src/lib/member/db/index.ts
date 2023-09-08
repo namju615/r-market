9b91bc5e137eaa7a43cb6b0ebc1d2a0d2f158026
type-huey <sanghun33@naver.com>
2023-09-08 15:35:29 +0900

    chore: commit test

--- src/lib/member/db/index.ts
+++ src/lib/member/db/index.ts
@@ -1,6 +1,5 @@
 import { SupabaseInstanse } from '$lib/supabase/supabaseService';
 
-// request id = 18 country data
 const member = await SupabaseInstanse().getClient().rpc('get_member').eq('id', 18);
 
 export default member;
